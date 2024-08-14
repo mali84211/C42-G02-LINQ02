33bdd3c923210edb02c67789cfee436ebf3d10c0
mali84211 <mali84211@gmail.com>
2024-08-14 20:16:00 +0300

    V1

--- C42-G02-LINQ02/Program.cs
+++ C42-G02-LINQ02/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace C42_G02_LINQ02
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace C42_G02_LINQ02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region V1
+            //var Result=  ProductList.Where(P => P.UnitsInStock == 0);
+
+            //  Result=from P in ProductList
+            //         where P.UnitsInStock == 0
+            //         select P;
+
+            //var Result = ProductList.Where(P => P.Category == "Meat/Poultry"&&P.UnitsInStock>0);
+
+            //Result = from P in ProductList
+            //         where P.UnitsInStock > 0 && P.Category == "Meat/Poultry"
+            //         select P;
+
+            //var Result = ProductList.Where((P,I) => P.UnitsInStock == 0&&I <20);
+            //var Result = ProductList.Where((P, I) => P.UnitsInStock > 0 && I < 5);
+
+            //ArrayList list = new ArrayList() {"mo",1,"ali",2,"ahmed",1.5,23,"M" };
+
+            //var Result= List.OfType<int>();
+            //var Result = List.OfType<string>();
+            //var Result = List.OfType<double>();
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
         }
     }
 }
