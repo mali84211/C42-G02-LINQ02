0b50505de7a79a16fb578849f37e15243c5bf8bd
mali84211 <mali84211@gmail.com>
2024-08-14 19:37:06 +0300

    Add project files.

---
+++ C42-G02-LINQ02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C42_G02_LINQ02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
